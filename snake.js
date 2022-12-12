08230637db114867c6e014dd6636edd18cf4bbc3
Anees <110382879+aneeschopra@users.noreply.github.com>
2022-12-12 16:53:10 -0500

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,25 @@
+
+//https://www.youtube.com/watch?v=bG2BmmYr9NQ//
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter=0;
+function jump(){
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
+}
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        block.style.animation = "none";
+        alert("You killed me! :( it's going to go faster so GET READY!!!  score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    }
+}, 10);
